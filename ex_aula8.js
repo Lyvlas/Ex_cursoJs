61a40cf323f042dee9dc42f9922b0d3132ccc55b
Paulo Setsuo <paulo.setsuojr@gmail.com>
2025-04-22 09:45:50 -0300

    Exemplo Filmes

---
+++ ex_aula8.js
@@ -0,0 +1,10 @@
+
+let filme = {
+    titulo: "Jogo da imitação",
+    nota: 8.0,
+    duracao: 114,
+    anoPublicacao: 2014,
+    genero: ["Biografia", "Drama", "Suspense", "Guerra"] 
+}
+
+console.table(filme)
